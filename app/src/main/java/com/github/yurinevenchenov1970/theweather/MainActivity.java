2058d04febd1566b015971dc5c5450100f66a93b
Yuri Nevenchenov <yurinevenchenov1970@gmail.com>
2017-10-24 23:04:39 +0300

    added UI components via ButterKnife

--- app/src/main/java/com/github/yurinevenchenov1970/theweather/MainActivity.java
+++ app/src/main/java/com/github/yurinevenchenov1970/theweather/MainActivity.java
@@ -1,13 +1,77 @@
 package com.github.yurinevenchenov1970.theweather;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.city_edit_text)
+    TextView mCityTextView;
+
+    @BindView(R.id.seek_bar)
+    SeekBar mSeekBar;
+
+    @BindView(R.id.forecast_length_text_view)
+    TextView mForecastLengthTextView;
+
+    @BindView(R.id.forecast_length_end_text_view)
+    TextView mForecastLengthEndTextView;
+
+    @BindView(R.id.weather_list_view)
+    ListView mWeatherListView;
+
+    private int mForecastLength;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initUI();
+    }
+
+    private void initUI() {
+        mSeekBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    int progress = 0;
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar,
+                                                  int progressValue, boolean fromUser) {
+                        progress = progressValue;
+                        mForecastLength = progress;
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        fillForecastLength(progress);
+                    }
+                });
+    }
+
+    private void fillForecastLength(int progress){
+        mForecastLengthTextView.setText(String.valueOf(progress));
+        correctDayWordForm(progress);
+    }
+    private void correctDayWordForm(int progress) {
+        if (progress == 1) {
+            mForecastLengthEndTextView.setText(R.string.day);
+        } else if (progress == 2 || progress == 3 || progress == 4) {
+            mForecastLengthEndTextView.setText(R.string.day_2_4);
+        } else if (progress >= 5 || progress == 0) {
+            mForecastLengthEndTextView.setText(R.string.days);
+        }
     }
 }
